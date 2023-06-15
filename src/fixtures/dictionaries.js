d9bdc7e3c688d8c4171fa9b4d5c72afca0fe2d04
James Ortiz <jameswortiz728@gmail.com>
2023-06-14 21:06:19 -0400

    Updated dictionary

--- src/fixtures/dictionaries.js
+++ src/fixtures/dictionaries.js
@@ -1,7 +1,7 @@
-const basicDictionary = ["turtle", "let", "const", "var", "parrot", "cat", "dog", "mouse", "frog", "short", "ratio", "the","of","and","a","to","in","is","you","that","it","he","was","for",
+const basicDictionary = ["turtle", "parrot", "cat", "dog", "mouse", "frog", "short", "ratio", "the","of","and","a","to","in","is","you","that","it","he","was","for",
 "on","are","as","with","his","they","I","at","be","this","have","from","or","one","had","by","word","but","not","what","all","were","we","when","your","can","said","there","use","an","each",
 "which","she","do","how","their","if","will","up","other","about","out","many","then","them","these","so","some","her","would","make","like","him","into","time","has","look","two","more",
 "write","go","see","number","no","way","could","people","my","than","first","water","been","call","who","oil","its","now","find","long","down","day","did","get","come","made","may","part",
-"quick", "brown", "fox", "jump", "over", "lazy", "skill", "export", "pizza", "flower", "issue", "hello", "world", "type", "test", "Java", "food", "drink","stats", "report", "thanks"];
+"quick", "brown", "fox", "jump", "over", "lazy", "skill", "export", "pizza", "flower", "issue", "hello", "world", "type", "test", "food", "drink","stats", "report", "thanks"];
 
 export {basicDictionary};
